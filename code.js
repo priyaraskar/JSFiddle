1fd7b66996873c81f9883450dbcb1777501f6d7f
priyaraskar <priyaraskar@gmail.com>
2019-02-20 19:05:40 -0800

    Firs Commit

---
+++ code.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $("#btnLoad").on("click", onMouseClick);
+  });
+  
+  function onMouseClick() {
+    $.ajax("sample.txt", {
+      success: setContent,
+      type: "GET",
+      datatype: "text"
+    });
+  }
+  
+  function setContent(data, status, jqxhr) {
+    $("#divFile").text(data);
+  }
+  
